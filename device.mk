ba203eac2fbbf0a839dfa209feb72ec53b448e03
David <daviiideveloper@gmail.com>
2022-02-16 10:07:51 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,22 +14,22 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
-#PRODUCT_PACKAGES += \
-#    android.hardware.boot@1.0-impl \
-#    android.hardware.boot@1.0-service
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
 
-# PRODUCT_PACKAGES += \
-#   bootctrl.sm6150
+ PRODUCT_PACKAGES += \
+   bootctrl.sm6150
 
-#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-#    bootctrl.sm6150 \
-#    libgptutils \
-#   libz \
-#   libcutils
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.sm6150 \
+    libgptutils \
+   libz \
+   libcutils
 
-#PRODUCT_PACKAGES += \
-#    otapreopt_script \
-#    cppreopts.sh \
-#    update_engine \
-#    update_verifier \
-#    update_engine_sideload
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
